456034576c3b41f12673854c4886c3016b63aa9a
Quentin384 <zampieri.quentin38@gmail.com>
2025-06-30 10:09:32 +0200

    fixing verify email route bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,14 @@
-//import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-
-//@Component({
-  //selector: 'app-root',
-  //imports: [RouterOutlet],
-  //templateUrl: './app.component.html',
-  //styleUrl: './app.component.scss'
-//})
-//export class AppComponent {
-//  title = 'GroupGenerateur';
-//}
 import { Component } from '@angular/core';
-import { VerifyEmailComponent } from './pages/verify-email/verify-email';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [VerifyEmailComponent],
-  template: `<app-verify-email />`
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'GroupGenerateur';
+}
+
 
