aea3a8578dd6a9abc3bf55111b96ef1d7e221d6d
Raidel-28 <llefebvre2838@outlook.fr>
2025-07-02 16:03:09 +0200

    Add page generate group and router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { Home } from './pages/home/home.component';
 import { Members } from './pages/members/members.component';
 import { VerifyEmailComponent } from './pages/verify-email/verify-email';
 import { ListComponent } from './pages/list/list.component';
-
+import { GenerateGroupsComponent } from './pages/generate-groups/generate-groups.component';
 
 
 export const routes: Routes = [
@@ -21,4 +21,5 @@ export const routes: Routes = [
   { path: 'members', component: Members, data: { title: 'Members' } },
   { path: 'verifyemail', component: VerifyEmailComponent, data: { title: 'Verify Email' } },
   { path: 'list', component: ListComponent, data: { title: 'List' } },
+  { path: 'generate-groups', component: GenerateGroupsComponent, data: { title: 'Generate Groups' } },
 ];
