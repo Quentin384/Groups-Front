836c7b5b4c138bd43c30329a399ab608a837dfbd
Quentin384 <zampieri.quentin38@gmail.com>
2025-07-02 07:55:24 +0200

    dynamique title in header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BackButtonComponent } from '../../shared/components/back.button/back.button.component';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { BackButtonComponent } from '../components/back.button/back.button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    CommonModule,
-    BackButtonComponent     
-  ],
+  imports: [ BackButtonComponent ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class Header { }
+export class Header {
+  title = '';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.router.events.pipe(
+      filter(evt => evt instanceof NavigationEnd),
+      map(() => {
+        // descendre jusqu’à la route enfant la plus profonde
+        let route = this.activatedRoute.root;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(r => r.outlet === 'primary'),
+      map(r => r.snapshot.data['title'] as string)
+    ).subscribe(t => this.title = t);
+  }
+}
