8661a068b8112313e587d58fe9c3bb14c62ea9b9
Quentin384 <zampieri.quentin38@gmail.com>
2025-07-03 11:17:30 +0200

    loader on groupe and admin page + button with router link

--- src/app/pages/generate-groups/generate-groups.component.ts
+++ src/app/pages/generate-groups/generate-groups.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink }                 from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AlertComponent } from '../../shared/components/alert/alert.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+
 
 @Component({
   selector: 'app-generate-groups',
-  imports: [ CommonModule, FormsModule, AlertComponent ],
+  imports: [ CommonModule, FormsModule, AlertComponent, ButtonComponent, RouterLink ],
   templateUrl: './generate-groups.component.html',
   styleUrl: './generate-groups.component.scss'
 })
